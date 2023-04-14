782627b8d5335676a78fec13c5de179fe4449cda
Pravin Bhagwat <85389806+pravinbhagwat@users.noreply.github.com>
2023-04-14 17:21:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+const element = document.getElementById("level");
+let level = 0;
+while (element) {
+  level++;
+  element = element.parentNode;
+}
+alert("The DOM level of element is:",level);
